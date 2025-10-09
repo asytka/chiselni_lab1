19ef04e8abc6460337f158ca175729533e9d6191
asytka <asyt2203@gmail.com>
2025-10-09 17:43:17 +0300

    made the analitical solution more precise

--- chiselni_lab1.cpp
+++ chiselni_lab1.cpp
@@ -1,81 +1,4 @@
-﻿//#include <iostream>
-//#include <cmath>
-//#include <iomanip>
-//
-//double f(double t, double y) {
-//    double f1 = std::exp(-t * t) * std::cos(0.8 * t);
-//    double f2 = std::exp(2 * t - 3);
-//    double g = 2 * (t - 2);
-//    return f1 * f2 - g * y;
-//}
-//
-//double rk4_step(double t, double y, double h) {
-//    double k1 = f(t, y);
-//    double k2 = f(t + h / 2.0, y + h * k1 / 2.0);
-//    double k3 = f(t + h / 2.0, y + h * k2 / 2.0);
-//    double k4 = f(t + h, y + h * k3);
-//    return y + h * (k1 + 2 * k2 + 2 * k3 + k4) / 6.0;
-//}
-//
-//double rk4_adaptive(double& t, double& y, double& h,
-//    double eps = 1e-5, double epsM = 1e-6) {
-//    while (true) {
-//        double y_big = rk4_step(t, y, h);
-//
-//        double y_half = rk4_step(t, y, h / 2.0);
-//        y_half = rk4_step(t + h / 2.0, y_half, h / 2.0);
-//
-//        double error = std::fabs(y_half - y_big) / 15.0;
-//
-//        if (error < eps) {
-//            t += h;
-//            y = y_half;
-//
-//            double factor = pow(eps / (error + 1e-16), 0.25); 
-//            if (factor > 2.0) factor = 2.0;  
-//            if (factor < 0.5) factor = 0.5;  
-//            h *= factor;
-//
-//            return error;
-//        }
-//        else {
-//            h *= 0.5;
-//        }
-//    }
-//}
-//
-//
-//int main() {
-//    double t0 = 1.0;
-//    double T = 6.0;
-//    double y = 10.0;
-//    double h = 0.1;   
-//    double eps = 1e-5;
-//    double epsM = 1e-6;
-//    double solution = 0.000003066334874;
-//
-//    std::cout << std::fixed << std::setprecision(10);
-//
-//    int steps = 0;
-//    double t = t0;
-//
-//    std::cout << "t=" << t << " y=" << y << " u(t)=" << rk4_adaptive(t, y, h, eps, epsM) << " |y-u(t)|=" << y - rk4_adaptive(t, y, h, eps, epsM) << "\n";
-//    while (t < T) {
-//        double error = rk4_adaptive(t, y, h, eps, epsM);
-//        std::cout << "t=" << t << " y=" << y << " u(t)=" << rk4_adaptive(t, y, h, eps, epsM) << " |y-u(t)|=" << y - error << "\n";
-//        if (t + h > T) h = T - t;
-//        steps++;
-//    }
-//
-//    std::cout << "\nFinal result: y(" << T << ") = " << y << "\n";
-//    std::cout << "Maple result: " << solution << std::endl;
-//    std::cout << "Ammount of steps: " << steps << std::endl;
-//    double error = std::fabs(solution - y);
-//    std::cout << "\nError: " << std::scientific << error << std::fixed << " (" << error << ")\n";
-//    return 0;
-//}
-
-#include <iostream>
+﻿#include <iostream>
 #include <cmath>
 #include <iomanip>
 #include <algorithm>
@@ -88,17 +11,15 @@ double f(double t, double y) {
 }
 
 double solution(double t) {
-    // t1 — частина з cos і sin
     double t1 = std::exp(-t * t + 2.0 * t - 3.0) *
         (-25.0 / 58.0 * std::cos(0.8 * t)
             + 5.0 / 29.0 * std::sin(0.8 * t));
 
-    // t2 — друга частина, пов'язана з початковими умовами
-    double A = -10.0
-        - (25.0 / 28.0) * std::cos(0.8)
-        + (5.0 / 29.0) * std::sin(0.8);
+    double A = -10.0 + std::exp(-2)
+        * ( - (25.0 / 58.0) * std::cos(0.8) + 
+            (5.0 / 29.0) * std::sin(0.8));
 
-    double t2 = (std::exp(-t * t + 4.0 * t) * A) / std::exp(5);
+    double t2 = (std::exp(-t * t + 4.0 * t) * A) / std::exp(3);
 
     return t1 - t2;
 }
